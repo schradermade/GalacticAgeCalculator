76151954f23daca9ac169e9346f79e9f63037e97
Nathan Schrader <nathanschrader@icloud.com>
2020-11-13 11:09:57 -0800

    refactor Spec #2 to include template literal

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -3,15 +3,14 @@ import { Age } from './../src/js/age.js';
 describe('Age', () => {
 
   test('should correctly take in user age and desired life expectancy', () => {
-    let userAge = new Age("35", "100");
-    expect(userAge.earthAge).toEqual("35");
-    expect(userAge.lifeExp).toEqual("100");
+    let userAge = new Age(35, 100);
+    expect(userAge.earthAge).toEqual(35);
+    expect(userAge.lifeExp).toEqual(100);
   });
 
   test('should correctly output users age and expected years left to live, refactored in Mercury years', () => {
-    let mercuryStats = new Age("35", "100");
-    expect(mercuryStats.mercuryAge()).toEqual("8.4");
-    expect(mercuryStats.mercuryAge()).toEqual("24");
+    let mercuryStats = new Age(35, 100);
+    expect(mercuryStats.mercury()).toEqual("Your Mercury stats: Age - 8.4, Expected life - 24, Years left to live - 15.6");
   });
 
 });
