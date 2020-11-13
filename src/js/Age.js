dcf2182959c3742cd7caf47c3cebb2cd49b6c655
Nathan Schrader <nathanschrader@icloud.com>
2020-11-13 11:47:50 -0800

    add mars() method - to match Spec #4 - Passed with 100% coverage

--- src/js/Age.js
+++ src/js/Age.js
@@ -1,12 +1,37 @@
 export class Age  {
-  constructor(earthYears, lifeExp)  {
-    this.earthYears = earthYears;
-    this.lifeExp = lifeExp;
-  }
   
+  constructor(earthAge, lifeExp)  {
+    this.earthAge = parseInt(earthAge);
+    this.lifeExp = parseInt(lifeExp);
+  }
+
+  mercury()  {
+    this.ageRefactor = this.earthAge * .24;
+    this.expRefactor = this.lifeExp * .24;
+    this.mercuryAge = this.ageRefactor;
+    this.mercuryExp = this.expRefactor;
+    this.yearsLeft = this.expRefactor - this.ageRefactor;
+    return `Your Mercury stats: Age - ${this.mercuryAge}, Expected life - ${this.mercuryExp}, Years left to live - ${this.yearsLeft}`;
+  }
+
+  venus()  {
+    this.ageRefactor = this.earthAge * .62;
+    this.expRefactor = this.lifeExp * .62;
+    this.venusAge = this.ageRefactor;
+    this.venusExp = this.expRefactor;
+    this.yearsLeft = this.expRefactor - this.ageRefactor;
+    return `Your Venus stats: Age - ${this.venusAge}, Expected life - ${this.venusExp}, Years left to live - ${this.yearsLeft}`;
+  }
+
+  mars()  {
+    //this.ageRefactor = this.earthAge * .62;
+    //this.expRefactor = this.lifeExp * .62;
+    //this.venusAge = this.ageRefactor;
+    //this.venusExp = this.expRefactor;
+    //this.yearsLeft = this.expRefactor - this.ageRefactor;
+    //return `Your Venus stats: Age - ${this.venusAge}, Expected life - ${this.venusExp}, Years left to live - ${this.yearsLeft}`;
+  }
 };
-
-
 
 
 
