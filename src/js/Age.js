0d937e7b203d0391aceab1f3ee096107f6fd97d6
Nathan Schrader <nathanschrader@icloud.com>
2020-11-13 18:08:20 -0800

    refactored code to DRY

--- src/js/Age.js
+++ src/js/Age.js
@@ -4,55 +4,44 @@ export class Age  {
     this.earthAge = parseInt(earthAge);
     this.lifeExp = parseInt(lifeExp);
     this.yearsLeft = this.yearsDynamic();
-    this.factor = 0;
   }
 
   yearsDynamic() {
     if (this.earthAge <= this.lifeExp) {
-      this.yearsLeft = `Your remaining life is about`;
-      return this.yearsLeft;
+      return this.yearsLeft = `Your remaining life is about`;
     } else  {
-      this.yearsLeft = `You have outlived your life expectancy by`;
-      return this.yearsLeft;
+      return this.yearsLeft = `You have outlived your life expectancy by`;
     };
   }
 
   mercury()  {
     this.factor = .24;
-    this.ageRefactor = this.earthAge * this.factor;
-    this.expRefactor = this.lifeExp * this.factor;
-    this.mercuryAge = this.ageRefactor;
-    this.mercuryExp = this.expRefactor;
+    this.mercuryAge = this.earthAge * this.factor;
+    this.mercuryExp = this.lifeExp * this.factor;
     this.mercuryAbs = Math.abs(this.mercuryExp - this.mercuryAge);
     return `Your Mercury stats: Age - ${this.mercuryAge}, Expected life - ${this.mercuryExp}, ${this.yearsLeft} ${this.mercuryAbs} years!`;
   }
 
   venus()  {
     this.factor = .62;
-    this.ageRefactor = this.earthAge * this.factor;
-    this.expRefactor = this.lifeExp * this.factor;
-    this.venusAge = this.ageRefactor;
-    this.venusExp = this.expRefactor;
+    this.venusAge = this.earthAge * this.factor;
+    this.venusExp = this.lifeExp * this.factor;
     this.venusAbs = Math.abs(this.venusExp - this.venusAge);
     return `Your Venus stats: Age - ${this.venusAge}, Expected life - ${this.venusExp}, ${this.yearsLeft} ${this.venusAbs} years!`;
   }
 
   mars()  {
     this.factor = 1.88
-    this.ageRefactor = this.earthAge * this.factor;
-    this.expRefactor = this.lifeExp * this.factor;
-    this.marsAge = this.ageRefactor;
-    this.marsExp = this.expRefactor;
+    this.marsAge = this.earthAge * this.factor;
+    this.marsExp = this.lifeExp * this.factor;
     this.marsAbs = Math.abs(this.marsExp - this.marsAge);
     return `Your Mars stats: Age - ${this.marsAge}, Expected life - ${this.marsExp}, ${this.yearsLeft} ${this.marsAbs} years!`;
   }
 
   jupiter()  {
     this.factor = 11.86;
-    this.ageRefactor = (this.earthAge * this.factor);
-    this.expRefactor = (this.lifeExp * this.factor);
-    this.jupiterAge = Math.round(10 * this.ageRefactor)/10;
-    this.jupiterExp = Math.round(10 * this.expRefactor)/10;
+    this.jupiterAge = Math.round(10 * (this.earthAge * this.factor))/10;
+    this.jupiterExp = Math.round(10 * (this.lifeExp * this.factor))/10;
     this.jupiterAbs = Math.abs(this.jupiterExp - this.jupiterAge);
     return `Your Jupiter stats: Age - ${this.jupiterAge}, Expected life - ${this.jupiterExp}, ${this.yearsLeft} ${this.jupiterAbs} years!`;
   }
