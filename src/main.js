5de99d8db48286a31025496c4b18b78b4c0c7938
Nathan Schrader <nathanschrader@icloud.com>
2020-11-13 18:42:56 -0800

    add 'let age = new Age();' to main.js to resolve error at run-time-build

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Age }  from './../js/Age.js';
+import { Age } from './js/Age.js';
 // import Post from './js/post.js';
+
+let age = new Age();
+age;
