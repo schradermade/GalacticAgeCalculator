dbd126ca459fc47263476670ed615d08bd4480a3
Nathan Schrader <nathanschrader@icloud.com>
2020-11-17 18:09:36 -0800

    clean up unnecessary code in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { Age } from './js/Age.js';
-// import Post from './js/post.js';
 
 let age = new Age();
-age;
+
